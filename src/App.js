776a74b1649d39c961185f098d8774868ad5300e
zandeepsanand <sandeepsssanand@gmail.com>
2022-06-24 17:43:01 +0530

    static json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header/Header';
+import Sidebar from './components/sidebar/Sidebar';
+import {Container} from "react-bootstrap";
+import HomeScreen from './screens/homeScreen/HomeScreen';
+import "./_app.scss"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <div className='app__container border'>
+        <Sidebar/>
+        <Container fluid className="app__main border ">
+          <HomeScreen/>
+        </Container>
+      </div>
+
     </div>
   );
 }
